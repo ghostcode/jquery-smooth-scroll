af2ba88904ebe9bcfefb7b5663159a706da5906c
zhuxinyong <ghostcode521@gmail.com>
2017-07-17 19:16:50 +0800

    edit

--- jquery.smooth-scroll.js
+++ jquery.smooth-scroll.js
@@ -6,21 +6,43 @@
                 direction: 'v' // v: vertical h: horizontal
             },
             self = this,
-            $body = $(document.body)
+            $body = $(document.body),
+            $targets = self.find('a[href*="#"]')
 
         opts = $.extend({}, defaults, opts)
 
-        return self.find('a[href*="#"]').click(function(event) {
-            var hash = this.hash,
+        $(window).scroll(function() {
+            let $top = $(window).scrollTop();
+
+            if (self.flag) return
+
+            self.flag = setTimeout(function() {
+                $targets.each(function() {
+                    let hash = this.hash,
+                        $hash = $(this.hash)
+
+                    if ($top >= $hash.scrollTop()) {
+                        $targets.removeClass('xxx')
+                        $(`a[href="${hash}"]`).addClass('xxx');
+                        console.log(1111)
+                        return false
+                    }
+                })
+                self.flag = null
+            }, 200)
+
+        })
+
+        return $targets.click(function(event) {
+            let hash = this.hash,
                 $hash = $(hash);
 
             event.preventDefault()
 
             $body.stop().animate({
                 'scroll-top': $hash.offset().top - opts.offset
-            }, opts.speed, function() {
-                console.log('ok')
-            })
+            }, opts.speed, function() {})
         })
+
     }
 }(document, jQuery))
